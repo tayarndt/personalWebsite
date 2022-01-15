5aa03078339dc81970f8e9784a030c34ad8fef0e
Taylor Arndt <tarndt@accessible360.com>
2022-01-14 20:43:25 -0600

    Added blank about app

--- personalWebsite/personalWebsite/settings.py
+++ personalWebsite/personalWebsite/settings.py
@@ -74,6 +74,7 @@ WSGI_APPLICATION = 'personalWebsite.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
